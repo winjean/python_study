d5c01597782116407f32c76215a82d5eba3922c3
winjean <19548901@qq.com>
2020-05-29 17:34:06 +0800

    first commit

--- crawler/NumpyUtils.py
+++ crawler/NumpyUtils.py
@@ -1,18 +1,23 @@
 import numpy as np
 
 a = np.arange(12).reshape(3, 2, 2)
-print(a)
-print(a[0])
+print(f"a = {a}\n")
+
+print(f"a[0] = {a[0]}\n")
 
 # 数组的维度
-print(a.shape)
+print(f"shape = {a.shape}\n")
 
 # 数组轴的个数，轴的个数被称作秩 =shape的长度
-print(a.ndim)
+print(f"ndim = {a.ndim}\n")
 
 # 数组中每个元素的字节大小
-print(a.itemsize)
+print(f"itemsize = {a.itemsize}\n")
 
 # 数组元素的总个数 =shape属性中元组元素的乘积
-print(a.size)
+print(f"size = {a.size}\n")
+
+print(f"min = {a.min()}\n")
+print(f"max = {a.max()}\n")
+
 
